fbef4718c6d6bc6f81c30ad9b662ca5b73828ef2
Daniel Fanjul <daniel.fanjul.alcuten@gmail.com>
2018-12-17 14:09:20 +0100

    Fix 2017/p7/

--- 2017/p7/p.go
+++ 2017/p7/p.go
@@ -1,43 +1,83 @@
-package p7
+package p
 
 import (
 	"strconv"
 	"strings"
 )
 
-type Node struct {
-	Name     string
-	Weight   int
-	Children []*Node
-}
-
-func Parse(ss []string) (nn map[string]*Node) {
-	nn = map[string]*Node{}
-	for _, s := range ss {
-		p := strings.Split(s, " -> ")
-		q := strings.Split(p[0], " ")
-		n := Node{Name: q[0]}
-		nn[n.Name] = &n
-	}
-	for _, s := range ss {
-		p := strings.Split(s, " -> ")
-		q := strings.Split(p[0], " ")
-		n := nn[q[0]]
-		i, j := strings.Index(q[1], "("), strings.Index(q[1], ")")
-		w, err := strconv.Atoi(q[1][i+1 : j])
-		if err != nil {
-			panic(err)
-		}
-		n.Weight = w
-		if len(p) > 1 {
-			for _, t := range strings.Split(p[1], ", ") {
-				c := nn[t]
-				if c == nil {
-					panic(t)
-				}
-				n.Children = append(n.Children, c)
+func P1(input []string) (output string) {
+	d, r := map[string][]string{}, map[string][]string{}
+	for _, in := range input {
+		split := strings.Split(in, " ")
+		m := split[0]
+		if len(split) > 2 {
+			for _, s := range split[3:] {
+				s = strings.Trim(s, ",")
+				d[m] = append(d[m], s)
+				r[s] = append(r[s], m)
 			}
 		}
 	}
+	for m := range d {
+		if len(r[m]) == 0 {
+			return m
+		}
+	}
 	return
+}
+
+func weight(d map[string][]string, w map[string]int, n string) (v int) {
+	v += w[n]
+	for _, n2 := range d[n] {
+		v += weight(d, w, n2)
+	}
+	return
+}
+
+func P2(input []string, root string) (output int) {
+	d, r, w := map[string][]string{}, map[string][]string{}, map[string]int{}
+	for _, in := range input {
+		split := strings.Split(in, " ")
+		m := split[0]
+		if len(split) > 2 {
+			for _, s := range split[3:] {
+				s = strings.Trim(s, ",")
+				d[m] = append(d[m], s)
+				r[s] = append(r[s], m)
+			}
+		}
+		i, err := strconv.Atoi(strings.Trim(split[1], "()"))
+		if err != nil {
+			panic(err)
+		}
+		w[m] = i
+	}
+	n := root
+	diff := 0
+	for {
+		w2nn := map[int][]string{}
+		for _, n2 := range d[n] {
+			w := weight(d, w, n2)
+			w2nn[w] = append(w2nn[w], n2)
+		}
+		if len(w2nn) == 2 {
+			for w, nn := range w2nn {
+				if len(nn) != 1 {
+					output = w
+				}
+			}
+			for k, nn := range w2nn {
+				if len(nn) == 1 {
+					n = nn[0]
+					diff = output - k
+				}
+			}
+			continue
+		}
+		if len(w2nn) == 1 {
+			output = w[n] + diff
+			return
+		}
+		panic(len(w2nn))
+	}
 }
--- 2017/p7/p_test.go
+++ 2017/p7/p_test.go
@@ -1,19 +1,29 @@
-package p7
+package p
 
 import (
 	"testing"
 )
 
-func TestS1(t *testing.T) {
-	Parse(S1)
+func TestP1_S(t *testing.T) {
+	if n := P1(S); n != "tknk" {
+		t.Error(n)
+	}
 }
 
-func TestP1(t *testing.T) {
-	defer func() {
-		n := recover()
-		if n != "areod" {
-			t.Error(n)
-		}
-	}()
-	Parse(P1)
+func TestP1_X(t *testing.T) {
+	if n := P1(X); n != "vvsvez" {
+		t.Error(n)
+	}
+}
+
+func TestP2_S(t *testing.T) {
+	if n := P2(S, "tknk"); n != 60 {
+		t.Error(n)
+	}
+}
+
+func TestP2_X(t *testing.T) {
+	if n := P2(X, "vvsvez"); n != 362 {
+		t.Error(n)
+	}
 }
